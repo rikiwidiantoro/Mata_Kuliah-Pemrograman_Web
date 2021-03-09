03c5dac7947f1a54eebf65e0b3a8495f89c2ddd8
Riki Widiantoro <77282337+Riki-Toro@users.noreply.github.com>
2021-03-09 11:19:41 +0700

    Create script.js

---
+++ Tugas_4-landing_bootstrap/js/script.js
@@ -0,0 +1,24 @@
+// membuat event ketika join us
+const tangkapJoin = document.querySelector('.navbar-nav .tombol');
+
+tangkapJoin.addEventListener('click', function() {
+	
+	let nama = prompt('Masukkan nama Anda: ');
+
+	if ( true ) {
+		alert(`Selamat Datang ${nama} !`);
+	} else if (false){
+		alert('Anda belum memasukkan nama!');
+	}
+
+	// if ( click === true ) {
+		
+	// } else {
+	// 	alert('Anda belum memasukkan nama!');
+	// }
+
+	
+});
+
+
+// console.log(tangkapJoin);
