2a9fceeaaa309efd9d683440fddceeec56673a8c
Riki Widiantoro <77282337+Riki-Toro@users.noreply.github.com>
2021-03-12 08:28:23 +0700

    Update script.js

--- Tugas_4-landing_bootstrap/js/script.js
+++ Tugas_4-landing_bootstrap/js/script.js
@@ -1,4 +1,7 @@
-// membuat event ketika join us menggetikkan nama
+// Nama : Riki Widiantoro & Savina Lutfia N.
+// Kelompok 6 | Kelas 6C | Teknik Informatika
+
+// 1. membuat event ketika join us menggetikkan nama
 const tangkapJoin = document.querySelector('.navbar-nav .tombol');
 
 tangkapJoin.addEventListener('click', function(e) {
@@ -7,8 +10,8 @@ tangkapJoin.addEventListener('click', function(e) {
 
 	if (e = nama) {
 		alert(`Selamat Datang ${nama} !`); 
-	} else if(e = 'number') {
-		alert('Yang anda masukkan bukan nama!');
+	// } else if(e = 'number') {
+	// 	alert('Yang anda masukkan bukan nama!');
 	} else {
 		alert(`Anda belum memasukkan nama!`);
 	}
@@ -17,7 +20,7 @@ tangkapJoin.addEventListener('click', function(e) {
 
 
 
-// membuat text pada tag p pada testimonial berjalan sendiri dengan menambahkan tag marquee di javascript
+// 2. membuat text pada tag p pada testimonial berjalan sendiri dengan menambahkan tag marquee di javascript
 const jalan = document.createElement('marquee');
 const pM = document.querySelector('.testimonial p');
 const bungkus = document.querySelector('.testimonial .col-lg-8');
@@ -26,11 +29,52 @@ jalan.appendChild(pM);
 
 
 
-// membuat efek hover pada 3 gambar footer ketika di sorot gambar diberi css transform scale & translate
+// 3. membuat efek hover pada 3 gambar dibawah ketika di sorot gambar diberi css transform translate
 const gfoot =document.querySelectorAll('.d-flex img');
 gfoot[0].style.cursor = 'pointer';
 gfoot[1].style.cursor = 'pointer';
 gfoot[2].style.cursor = 'pointer';
+// efek hover nya pakai css
 
-gfoot[1].style:hover.transform: 'scale(-20px)';
+
+// 4. membuat background cover pada jumbotron tetap tetap di css >> dengan menambahkan properti background-Attachment dengan value fixed
+const bg = document.querySelector('.jumbotron');
+bg.style.backgroundAttachment = 'fixed';
+
+
+
+// 5. membuat tulisan di jumbotron mempunyai efek parallax dari atas turun kebawah ketika halaman di refresh
+// const tulisanCover = bg.querySelector('display-4');
+// tulisanCover.addEventListener('load', function() {
+// 	tulisanCover.addClass = '.tMuncul';
+// });
+
+$(window).on('load', function() {
+	$('.display-4').addClass('tMuncul');
+
+	// 6. membuat 3 info panel muncul satu per satu dengan efek parallax
+	$('.info-panel .y').each(function(i) {
+		setTimeout(function() {
+			$('.info-panel .y').eq(i).addClass('iMuncul');
+		}, 500 * (i + 1));
+	});
+});
+
+
+
+// 7. membuat efek parallax pada bagian workingspace >> gambar baru muncul ketika cursor hampir mencapai area dari workingspace
+$(window).scroll(function () {
+
+	let wScroll = $(this).scrollTop();
+
+	// 7. gambar baru muncul ketika cursor hampir mencapai area workingspace
+	if ( wScroll > $('.workingspace').offset().top - 400 ) {
+		$('.workingspace .img-fluid').addClass('gMuncul');
+
+		// 8. efek tulisan di samping img yaitu h2 & p baru muncul dari samping ketika cursor hampir mencapai area workingspce dengan koordinat sama seperti diatas
+		$('.workingspace h2').addClass('workingspaceMuncul');
+		$('.workingspace p').addClass('workingspaceMuncul');
+	}
+
+});
 
