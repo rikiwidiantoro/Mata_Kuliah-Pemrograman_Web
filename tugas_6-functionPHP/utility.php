38705a64395eaffa217e3ed51df5516ba14c6eb5
Riki Widiantoro <77282337+Riki-Toro@users.noreply.github.com>
2021-04-05 09:16:45 +0700

    Update utility.php

--- tugas_6-functionPHP/utility.php
+++ tugas_6-functionPHP/utility.php
@@ -1,7 +1,7 @@
 <?php
 // Nama = Riki Widiantoro & Savina Lutfia N
 // Kelompok 6 | kelas 6C
-echo "<h2>Riki Widiantoro & Savina Lutfia N | kelompok 6 | kelas 6C</h2>";
+echo "<h2>Riki Widiantoro (18532969) & Savina Lutfia N. (18532976) | kelompok 6 | kelas 6C</h2>";
 
 // count()
 // untuk mengetahui atau menghitung jumlah data yang terdapat dalam sebuah array
@@ -20,12 +20,12 @@ echo "<br><br><br><br>";
 
 
 // isset()
-// untuk memeriksa apakah suatu variabel sudah dibuat atau diatur atau belum.
+// untuk memeriksa apakah suatu variabel sudah dibuat atau diatur atau belum. dan jika di buat dalam pengkondisian maka hasilnya seperti berikut
 echo "<h2>isset()</h2>";
-if ( !isset($riki) ) {
-    echo "variabel riki belum dibuat!"; // jika ada tanda ! ini ditampilkan
+if ( !isset($riki) ) { // variabel riki belum dibuat sehingga nanti yang tampil pasti 'variabel belum dibuat!'
+    echo "variabel belum dibuat!"; // jika ada tanda ! ini ditampilkan
 } else {
-    echo "variabel riki sudah dibuat!"; // jika tidak ada tanda ! ini ditampilkan
+    echo "variabel sudah dibuat!"; // jika tidak ada tanda ! ini ditampilkan
 }
 
 
@@ -34,10 +34,13 @@ echo "<br><br><br><br>";
 
 
 // empty()
-// 
+// mirip seperti fuction isset(), tetapi fungsi nya agak berbeda yaitu untuk memriksa apakah suatu variabel bernilai kosong atau tidak,, dan jika di buat dalam pengkondisian seperti berikut
 echo "<h2>empty()</h2>";
+$widi = "";  // variabel widi dibuat tetapi nilainya tidak ada,, jadi yang tampil pasti 'variabel bernilai kosong!'.
 if( empty($widi) ) {
-    echo 
+    echo "variabel bernilai kosong!";
+} else {
+    echo "variabel ada nilainya!";
 }
 
 
