1788e67c6715adb60f720d453f2c10ff7bb9afa7
Riki Widiantoro <77282337+Riki-Toro@users.noreply.github.com>
2021-04-20 09:23:04 +0700

    Update functions.php

--- tugas_7-DBMS/functions.php
+++ tugas_7-DBMS/functions.php
@@ -1,4 +1,33 @@
 <?php
 // koneksi
 $koneksi = mysqli_connect("localhost", "root", "", "pemrogramanweb");
+
+// tampil
+function tampil($data) {
+    global $koneksi;
+    $result = mysqli_query($koneksi, $data);
+
+    $rows = [];
+    while( $row = mysqli_fetch_assoc($result) ) {
+        $rows[] = $row;
+    }
+
+    return $rows;
+}
+
+// tambah
+function tambah($data) {
+    global $koneksi;
+
+    // ambil data dari tiap" elemen di form tambah data
+    $nim = htmlspecialchars($data["nim"]);
+    $nama = htmlspecialchars($data["nama"]);
+    $alamat = htmlspecialchars($data["alamat"]);
+
+    // tambahkan data
+    $ambil = "INSERT INTO tugasdbms VALUES ('', '$nim', '$nama', '$alamat')";
+    mysqli_query($koneksi, $ambil);
+
+    return mysqli_affected_rows($koneksi);
+}
 ?>
