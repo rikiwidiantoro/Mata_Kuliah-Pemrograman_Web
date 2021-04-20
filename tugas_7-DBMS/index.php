b2f14bd515e0c118d2869002ee7bc6d16e631cdb
Riki Widiantoro <77282337+Riki-Toro@users.noreply.github.com>
2021-04-20 09:22:10 +0700

    Update index.php

--- tugas_7-DBMS/index.php
+++ tugas_7-DBMS/index.php
@@ -1,14 +1,9 @@
 <?php
 require "functions.php";
 
-// $nama = mysqli_query("SELECT * FROM tugasdbms");
-$result = mysqli_query($koneksi, "SELECT * FROM tugasdbms");
+$nama = tampil("SELECT * FROM tugasdbms");
 
-$rows = [];
-while( $row = mysqli_fetch_assoc($result) ) {
-    $rows[] = $row;
-}
-var_dump($rows);
+var_dump($nama);
 
 
 
@@ -20,11 +15,12 @@ var_dump($rows);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Administrasi Data</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
 </head>
 <body>
     <h1>Administrasi Data</h1>
 
-    <a href="">Tambah Data</a>
+    <a href="tambah.php">Tambah Data</a>
     <br><br>
 
     <table border="1" cellspacing="0" cellpadding="10">
@@ -35,9 +31,27 @@ var_dump($rows);
             <th>Alamat</th>
             <th>Menu</th>
         </tr>
-        <tr>
-
-        </tr>
+        
+        <?php $i = 1; ?>
+        <?php foreach($nama as $data) : ?>
+            <tr>
+                <td><?php echo $i; ?></td>
+                <td><?php echo $data['nim']; ?></td>
+                <td><?php echo $data['nama']; ?></td>
+                <td><?php echo $data['alamat']; ?></td>
+                <td>
+                    <a href="">edit</a>
+                    <a href="">hapus</a>
+                </td>
+            </tr>
+        <?php $i++; ?>
+        <?php endforeach; ?>
+        
     </table>
+
+    <br><br>
+    <footer>
+    <p class="footer">&copy Copyright 2021 | <i class="fab fa-github"></i> <a href="https://github.com/Riki-Toro" target="_blank">Riki-Toro</a>  |  <i class="fab fa-instagram"> @karya_rw</i></p>
+    </footer>
 </body>
 </html>
