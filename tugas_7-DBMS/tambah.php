d24876a4ba7388bfa91674b74b34b4530b1f29f1
Riki Widiantoro <77282337+Riki-Toro@users.noreply.github.com>
2021-04-22 12:29:54 +0700

    Update tambah.php

--- tugas_7-DBMS/tambah.php
+++ tugas_7-DBMS/tambah.php
@@ -1,4 +1,11 @@
 <?php
+session_start();
+
+// session
+if( !isset($_SESSION["login"]) ) {
+    header("location: login.php");
+}
+
 require 'functions.php';
 
 // cek apakah tombol tambah sudah ditekan apa belum
@@ -30,24 +37,37 @@ if( isset($_POST["tambah"]) ) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tambah Data</title>
+    <link rel="stylesheet" href="style.css">
 </head>
-<body>
+<body class="body-tambah">
     <h1>Tambah Data</h1>
+    <hr>
     <form action="" method="post">
-        <label for="nim">NIM : </label>
-        <input type="text" id="nim" name="nim" autocomplete="off" required>
-        <br>
-        <label for="nama">Nama : </label>
-        <input type="text" id="nama" name="nama" width="300px" autocomplete="off" required>
-        <br>
-        <label for="alamat">Alamat : </label>
-        <input type="text" id="alamat" name="alamat" autocomplete="off">
-        <br><br>
-        <button type="submit" name="tambah">Tambah Data</button>
+
+        <table border="0" cellspacing="0" cellpadding="5">
+            <tr>
+                <td><label for="nim">NIM</label></td>
+                <td>: <input type="text" id="nim" name="nim" autocomplete="off" required autofocus></td>
+            </tr>
+            <tr>
+                <td><label for="nama">Nama</label></td>
+                <td>: <input type="text" id="nama" name="nama" width="300px" autocomplete="off" required></td>
+            </tr>
+            <tr>
+                <td><label for="alamat">Alamat</label></td>
+                <td>: <input type="text" id="alamat" name="alamat" autocomplete="off"></td>
+            </tr>
+            <tr>
+                <td colspan="2" class="td-tambah-tambah">
+                    <button type="submit" name="tambah">Tambah Data</button>
+                </td>
+            </tr>
+        </table>
+
     </form>
 
     <br>
     <a href="index.php">Kembali</a>
-    <hr>
+    
 </body>
 </html>
