cb4ddc78595ac5739aeb58781d55ce27de32ef26
Riki Widiantoro <77282337+Riki-Toro@users.noreply.github.com>
2021-04-22 12:29:08 +0700

    Update index.php

--- tugas_7-DBMS/index.php
+++ tugas_7-DBMS/index.php
@@ -1,4 +1,11 @@
 <?php
+session_start();
+
+// session
+if( !isset($_SESSION["login"]) ) {
+    header("location: login.php");
+}
+
 require "functions.php";
 
 $nama = tampil("SELECT * FROM tugasdbms");
@@ -13,46 +20,58 @@ $nama = tampil("SELECT * FROM tugasdbms");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Administrasi Data</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-
-    <a href="logout.php">Logout</a>
-    <br>
-    
-    <h1>Administrasi Data Teknik Informatika 6C</h1>
-
-    <a href="tambah.php">Tambah Data</a>
-    <br><br>
-
-    <table border="1" cellspacing="0" cellpadding="10">
-        <tr>
-            <th>ID</th>
-            <th>NIM</th>
-            <th>Nama</th>
-            <th>Alamat</th>
-            <th>Menu</th>
-        </tr>
+    <div class="index-nav">
+        <a href="logout.php" class="index-logout">Logout</a>
+        <br>
         
-        <?php $i = 1; ?>
-        <?php foreach($nama as $data) : ?>
+        <h1 class="index-h1">Administrasi Data Mahasiswa Teknik Informatika 6C</h1>
+    </div>
+
+    <hr>
+
+    <div class="index-isi">
+
+        <div class="index-tambah">
+            <button><a href="tambah.php">Tambah Data</a></button>
+        </div>
+
+        <table border="1" cellspacing="0" cellpadding="10" class="index-tabel">
+
             <tr>
-                <td><?php echo $i; ?></td>
-                <td><?php echo $data['nim']; ?></td>
-                <td><?php echo $data['nama']; ?></td>
-                <td><?php echo $data['alamat']; ?></td>
-                <td>
-                    <a href="edit.php?id=<?php echo $data['id']; ?>">edit</a>
-                    <a href="hapus.php?id=<?php echo $data['id']; ?>" onclick="return confirm('Anda yakin ingin menghapus data <?= $data['nama']; ?>');">hapus</a>
-                </td>
+                <th>ID</th>
+                <th>NIM</th>
+                <th>Nama</th>
+                <th>Alamat</th>
+                <th>Menu</th>
             </tr>
-        <?php $i++; ?>
-        <?php endforeach; ?>
-        
-    </table>
+            
+            <?php $i = 1; ?>
+            <?php foreach($nama as $data) : ?>
+                <tr>
+                    <td><?php echo $i; ?></td>
+                    <td><?php echo $data['nim']; ?></td>
+                    <td><?php echo $data['nama']; ?></td>
+                    <td><?php echo $data['alamat']; ?></td>
+                    <td>
+                        <a href="edit.php?id=<?php echo $data['id']; ?>">edit</a> | 
+                        <a href="hapus.php?id=<?php echo $data['id']; ?>" onclick="return confirm('Anda yakin ingin menghapus data <?= $data['nama']; ?>');">hapus</a>
+                    </td>
+                </tr>
+            <?php $i++; ?>
+            <?php endforeach; ?>
+            
+        </table>
+    </div>
+
+    
 
     <br><br>
     <footer>
-    <p class="footer">&copy Copyright 2021 | <i class="fab fa-github"></i> <a href="https://github.com/Riki-Toro" target="_blank">Riki-Toro</a>  |  <i class="fab fa-instagram"> @karya.rw</i> |  <i class="fab fa-instagram"> @saviinalutfinas</i></p>
+        <p>Riki Widiantoro (18532969) | Savina Lutfia N.(18532976) | 6C</p>
+        <p class="footer">&copy 2021 | <i class="fab fa-github"></i> <a href="https://github.com/Riki-Toro" target="_blank">Riki-Toro</a>  |  <i class="fab fa-instagram"> @karya.rw</i> |  <i class="fab fa-instagram"> @saviinalutfinas</i></p>
     </footer>
 </body>
 </html>
