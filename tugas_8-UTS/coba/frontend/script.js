ea673b558a7c9ee253d44031484066715bf1895d
Riki Widiantoro <77282337+Riki-Toro@users.noreply.github.com>
2021-05-07 22:19:16 +0700

    Update script.js

--- tugas_8-UTS/coba/frontend/script.js
+++ tugas_8-UTS/coba/frontend/script.js
@@ -1,8 +1,24 @@
 function pilihanKomputer() {
     const comp = Math.random();
-    return comp;
-}
-console.log(pilihanKomputer());
 
+    if(comp < 0.33 || comp <= 0.34) {
+        return "GUNTING";
+    } else if(comp >= 0.35 && comp < 0.67) {
+        return "BATU";
+    } else {
+        return "KERTAS";
+    }
+
+}
+
+
+
+// ketika di klik
 const tombol = document.querySelectorAll('.btn');
-console.log(tombol);
+tombol.forEach(function(pilihan) {
+    pilihan.addEventListener('click', function() {
+        const hasil = pilihanKomputer();
+        const kotak = document.querySelector('.kotak');
+        kotak.innerHTML = hasil;
+    });
+});
