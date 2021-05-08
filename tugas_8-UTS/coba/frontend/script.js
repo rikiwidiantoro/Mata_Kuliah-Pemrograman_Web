3c5f3b232d00b52eb24f4adc675b300b59e536d5
Riki Widiantoro <77282337+Riki-Toro@users.noreply.github.com>
2021-05-08 12:07:59 +0700

    Update script.js

--- tugas_8-UTS/coba/frontend/script.js
+++ tugas_8-UTS/coba/frontend/script.js
@@ -1,24 +1,43 @@
+// fungsi untuk membuat computer memilih gunting, batu, atau kertas
 function pilihanKomputer() {
     const comp = Math.random();
 
-    if(comp < 0.33 || comp <= 0.34) {
+    if( comp < 0.33 || comp <= 0.34 ) {
         return "GUNTING";
-    } else if(comp >= 0.35 && comp < 0.67) {
+    } else if( comp >= 0.35 && comp < 0.67 ) {
         return "BATU";
     } else {
         return "KERTAS";
     }
+}
 
+
+// fungsi untuk pengkondisian user vs komputer itu menang atau kalah
+function hasil(comp, user) {
+    if( user == comp ) return "SERI!";
+    if( user == "GUNTING" ) return ( comp == "BATU" ) ? "KALAH!" : "MENANG!";
+    if( user == "BATU" ) return ( comp == "GUNTING" ) ? "MENANG!" : "KALAH!";
+    if( user == "KERTAS" ) return ( comp == "GUNTING" ) ? "KALAH!" : "MENANG!";
 }
 
 
 
-// ketika di klik
+// ketika 3 tombol di klik
 const tombol = document.querySelectorAll('.btn');
 tombol.forEach(function(pilihan) {
     pilihan.addEventListener('click', function() {
-        const hasil = pilihanKomputer();
+        const komputer = pilihanKomputer();
+        const User = pilihan.className;
+        console.log(User);
         const kotak = document.querySelector('.kotak');
-        kotak.innerHTML = hasil;
+        
+        kotak.innerHTML = komputer;
+        
+        
+        
+        // const pilihanUser = pilihan.className;
+        // const hasil = hasil(komputer, pilihanUser);
+        // const hasilnya = document.querySelector('.hasil');
+        // hasilnya.innerHTML = hasil;
     });
 });
