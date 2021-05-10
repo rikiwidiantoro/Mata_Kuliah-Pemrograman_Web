ba41ca8b5a7ec1e89cf5db551758557c5b7da2e8
Riki Widiantoro <77282337+Riki-Toro@users.noreply.github.com>
2021-05-10 21:45:52 +0700

    Update script.js

--- tugas_8-UTS/UTS-aplikasiWeb-game/js/script.js
+++ tugas_8-UTS/UTS-aplikasiWeb-game/js/script.js
@@ -2,15 +2,38 @@
 function pilihanKomputer() {
     const comp = Math.random();
 
-    if( comp < 0.33 || comp <= 0.34 ) {
+    if( comp > 0 && comp < 0.34 ^ comp == 0.5 ) { // bernilai TRUE >> (true, true, false) >> jika di isi comp < 0.3399 di setelah XOR maka akan bernilai true dan hasilnya akan false / kosong yg di tampilkan
         return "GUNTING";
-    } else if( comp >= 0.35 && comp < 0.67 ) {
+    } else if( comp >= 0.34 && comp < 0.67 || comp == 0.4) { // bernilai TRUE >> (true, true, false) >> jika di isi comp == 0.67 setelah AND maka akan bernilai false semua dan hasilnya akan false / kosong yg di tampilkan
         return "BATU";
-    } else {
+    } else if( comp >= 0.67 && comp <= 1 ) { // bernilai TRUE >> (true, true) >> jika di isi comp == 1 setelah AND maka akan bernilai false semua dan hasilnya akan false / kosong yg di tampilkan
         return "KERTAS";
+    } else {
+        return "KOSONG";
     }
 }
 
+// AND (&&) >> dianggap TRUE jika keduanya bernilai TRUE, dan jika keduanya FALSE atau salah satu FALSE maka bernilai FALSE
+// OR (||) >> dianggap TRUE jika keduanya TRUE dan salah satu saja bernilai TRUE, serta jika keduanya FALSE maka bernilai FALSE
+// XOR (^) >> exclusive OR dianggap TRUE jika salah satu saja bernilai TRUE, dan jika keduanya TRUE atau FALSE maka bernilai FALSE
+
+// let satu = 1;
+// let dua = 2;
+// let tiga = 3;
+
+// console.log(satu, dua, tiga);
+
+// if( satu == 1 && dua == 3 ^ tiga == 2) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// if( satu == 2 && dua == 1 ) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
 
 // fungsi untuk pengkondisian user vs komputer itu menang atau kalah
 function hasil(comp, user) {
