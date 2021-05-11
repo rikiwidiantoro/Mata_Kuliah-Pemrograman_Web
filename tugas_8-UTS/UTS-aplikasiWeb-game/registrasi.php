3eb4ad0034f33157b58b742c379fe82333025eb7
Riki Widiantoro <77282337+Riki-Toro@users.noreply.github.com>
2021-05-11 12:26:48 +0700

    Update registrasi.php

--- tugas_8-UTS/UTS-aplikasiWeb-game/registrasi.php
+++ tugas_8-UTS/UTS-aplikasiWeb-game/registrasi.php
@@ -38,12 +38,23 @@ if( isset($_POST['button']) ) {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
 
         <!-- my css -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
         <style>
             .container {
-                width: 450px;
+                width: 430px;
                 height: auto;
-                background-color: grey;
+                background-color: lightgrey;
                 border-radius: 10px;
+            }
+            .kembali {
+                display: block;
+                text-align: left;
+                margin-left: 10px;
+                font-size: 14px;
+            }
+            .footer {
+                font-size: 12px;
+                line-height: 5px;
             }
         </style>
 
@@ -53,26 +64,29 @@ if( isset($_POST['button']) ) {
 
         <form action="" method="post">
             <div class="mt-5 p-5 justify-content-center container">
-                <h3>Halaman Registrasi</h3>
+                <h3 class="text-center">Halaman Registrasi</h3>
                 <div class="input-group my-3 mt-4">
-                    <label class="input-group-text" for="username">Username</label>
-                    <input type="text" class="form-control" placeholder="" aria-label="username" aria-describedby="username" name="username" autocomplete="off" id="username" autofocus>
+                    <label class="input-group-text" for="username"><i class="fas fa-user"></i></label>
+                    <input type="text" class="form-control" placeholder="username" aria-label="username" aria-describedby="username" name="username" autocomplete="off" id="username" autofocus required>
                 </div>
                 <div class="input-group mb-3">
-                    <label class="input-group-text" for="password">Password</label>
-                    <input type="password" class="form-control" placeholder="" aria-label="password" aria-describedby="password" name="password" autocomplete="off" id="password">
+                    <label class="input-group-text" for="password"><i class="fas fa-lock"></i></label>
+                    <input type="password" class="form-control" placeholder="password" aria-label="password" aria-describedby="password" name="password" autocomplete="off" id="password" required>
                 </div>
                 <div class="input-group mb-4">
-                    <label class="input-group-text" for="password2">Ulangi Password</label>
-                    <input type="password" class="form-control" placeholder="" aria-label="password2" aria-describedby="password2" name="password2" autocomplete="off" id="password2">
+                    <label class="input-group-text" for="password2"><i class="fas fa-sync-alt"></i></label>
+                    <input type="password" class="form-control" placeholder="ulangi password" aria-label="password2" aria-describedby="password2" name="password2" autocomplete="off" id="password2" required>
                 </div>
                 <div class="d-grid gap-2">
-                    <button type="submit" name="button" class="btn btn-dark">Registrasi</button>
+                    <button type="submit" name="button" class="btn btn-dark mb-3">Registrasi</button>
                 </div>
 
-                <a href="login.php">kembali</a>
+                <a href="login.php" class="kembali"><i class="fas fa-long-arrow-alt-left"> kembali</i></a>
 
-                <p class="mt-3">&copy; 2021 | Riki Widiantoro | <a href="https://github.com/Riki-Toro" target="_blank" class="white-text"><i class="fab fa-github"></i> Riki-Toro </a> | <i class="fab fa-instagram"> @karya.rw</i></p>
+                <footer class="footer mt-5 text-center">
+                    <p>Riki Widiantoro (18532969) | Savina Lutfia N.(18532976) | 6C</p>
+                    <p>&copy; 2021 | <a href="https://github.com/Riki-Toro" target="_blank" class="white-text"><i class="fab fa-github"></i> Riki-Toro </a> | <i class="fab fa-instagram"> @karya.rw</i> | <i class="fab fa-instagram"> @saviinalutfinas</i></p>
+                </footer>
             </div>
         </form>
 
